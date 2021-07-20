d6c5c61b19b8afba4db08b4c706197d6f1e70512
Yongfeng LI <wliyongfeng@gmail.com>
2021-07-20 10:06:40 +0800

    Update

--- src/balance.js
+++ src/balance.js
@@ -21,10 +21,18 @@ async function main() {
   const api = await getApi();
   const address = '5Dw5KnvTs96FaRQFez1Su15XMMJ65QAi4F1ugNBaXUBiGbX6'
 
+  const preHeight = 109590
+  const preBlockHash = await api.rpc.chain.getBlockHash(preHeight);
+  const preAccount = await api.query.system.account.at(preBlockHash, address)
+  const preFree = preAccount.data.free.toNumber()
+
   const height = 109591
-  const blockHash = await api.rpc.chain.getBlockHash(height); // 38745
+  const blockHash = await api.rpc.chain.getBlockHash(height);
 
   const account = await api.query.system.account.at(blockHash, address)
+  const free = account.data.free.toNumber()
+
+  console.log(`free - preFree: ${free - preFree}`)
   console.log(account.toJSON())
 }
 
---
+++ src/teleport.js
@@ -0,0 +1,66 @@
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+let provider = null;
+let api = null;
+
+const defaultKsmEndPoint = "wss://kusama.elara.patract.io/";
+const dotEndPoint = "wss://polkadot.elara.patract.io/";
+const westmintEndPoint = "wss://westmint-rpc.polkadot.io/";
+
+async function getApi() {
+  if (!api) {
+    provider = new WsProvider(westmintEndPoint);
+    api = await ApiPromise.create({ provider });
+  }
+
+  return api;
+}
+
+async function main() {
+  const api = await getApi();
+
+  const height = 109591
+  const blockHash = await api.rpc.chain.getBlockHash(height); // 38745
+  const block = await api.rpc.chain.getBlock(blockHash)
+
+  // Downward Messages mean the messages from relay chain
+  const downwardMessages = block.block.extrinsics[0].method.args[0].get('downwardMessages')
+
+  // https://next.statescan.io/westmint/block/109591?tab=extrinsics
+  const pubMsg = downwardMessages[0].pubMsg
+
+  const versionedXcm = api.registry.createType('VersionedXcm', pubMsg, true)
+  if (!versionedXcm.isV0) {
+    return
+  }
+
+  const v0Xcm = versionedXcm.asV0
+  if (!v0Xcm.isTeleportAsset) {
+    return
+  }
+
+  const teleportAsset = v0Xcm.asTeleportAsset
+  for (const asset of teleportAsset.assets) {
+    if (!asset.isConcreteFungible) {
+      continue
+    }
+
+    const fungible = asset.asConcreteFungible
+    const id = fungible.id
+    if (isFromParent(id)) {
+      const amount = fungible.amount.toNumber()
+      console.log(`amount: ${amount}`)
+    }
+  }
+}
+
+function isFromParent(id) {
+  if (id.isX1) {
+    const x1 = id.asX1
+    return x1.isParent
+  }
+  return false
+}
+
+main().catch(console.error).finally(() => {
+  process.exit(0)
+});
