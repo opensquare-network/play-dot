3ed48e3c03b76d5b1e7df0076d47b5bcb54a2c15
Yongfeng LI <wliyongfeng@gmail.com>
2021-07-22 11:22:30 +0800

    Update

--- src/teleport.js
+++ src/teleport.js
@@ -38,8 +38,8 @@ async function main() {
     return
   }
 
-  const teleportAsset = v0Xcm.asTeleportAsset
-  for (const asset of teleportAsset.assets) {
+  const teleportAssetMessage = v0Xcm.asTeleportAsset
+  for (const asset of teleportAssetMessage.assets) {
     if (!asset.isConcreteFungible) {
       continue
     }
@@ -61,6 +61,35 @@ function isFromParent(id) {
   return false
 }
 
-main().catch(console.error).finally(() => {
+//
+async function main2() {
+  const api = await getApi();
+
+  const height = 216955
+  const blockHash = await api.rpc.chain.getBlockHash(height);
+  const block = await api.rpc.chain.getBlock(blockHash);
+
+  // Downward Messages mean the messages from relay chain
+  const downwardMessages = block.block.extrinsics[0].method.args[0].get('downwardMessages')
+
+  // https://next.statescan.io/westmint/block/109591?tab=extrinsics
+  const pubMsg = downwardMessages[0].pubMsg
+
+  const versionedXcm = api.registry.createType('VersionedXcm', pubMsg, true)
+  if (!versionedXcm.isV0) {
+    return
+  }
+
+  const v0Xcm = versionedXcm.asV0
+  if (!v0Xcm.isTeleportAsset) {
+    return
+  }
+
+  const teleportAsset = v0Xcm.asTeleportAsset
+  console.log(teleportAsset)
+
+}
+
+main2().catch(console.error).finally(() => {
   process.exit(0)
 });
