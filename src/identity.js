fc5c2f2975c686780d45c954101162446f05ba51
Yongfeng LI <wliyongfeng@gmail.com>
2021-08-11 13:09:54 +0800

    Update

--- src/identity.js
+++ src/identity.js
@@ -1,6 +1,7 @@
 const { ApiPromise, WsProvider } = require("@polkadot/api");
 let provider = null;
 let api = null;
+const { xxhashAsHex } = require("@polkadot/util-crypto")
 
 const defaultKsmEndPoint = "wss://kusama.elara.patract.io/";
 const dotEndPoint = "wss://polkadot.elara.patract.io/";
@@ -17,12 +18,33 @@ async function getApi() {
 }
 
 async function main() {
-  const api = await getApi();
+  await getApi();
+  await getKeys();
+  // await getInfo();
+}
+
+async function getInfo() {
   const address = '1363HWTPzDrzAQ6ChFiMU6mP4b6jmQid2ae55JQcKtZnpLGv'
 
   const info = await api.derive.accounts.info(address);
   const subs = await api.query.identity?.subsOf(address)
   console.log(info, subs)
+}
+
+async function getKeys() {
+  // await api.query.identity.identityOf
+
+  // const modulePrefix = xxhashAsHex('bounties')
+  // console.log('modulePrefix', modulePrefix)
+  //
+  // const keyPrefix = xxhashAsHex('bounties')
+  // console.log('keyPrefix', keyPrefix)
+
+  const entries = await api.query.identity.identityOf.entries()
+  console.log(entries)
+
+  const superInfo = await api.query.identity.superOf.entries()
+  console.log(superInfo)
 }
 
 main().catch(console.error).finally(() => {
