57de5ed59bed628a6a1ae09c21a6e23697ff695f
Yongfeng LI <wliyongfeng@gmail.com>
2021-08-12 09:59:54 +0800

    Update

--- src/storage/storageKey.js
+++ src/storage/storageKey.js
@@ -5,7 +5,6 @@ const section = xxhashAsU8a('System', 128)
 const method = xxhashAsU8a('Account', 128)
 
 function main() {
-
   const addr = 'DMHW1yWZS4qingUwcJRjZSJ8SvbvMUEKZL1oMiwcUXHBGWA';
   const id = decodeAddress(addr);
   console.log(u8aToHex(id))
