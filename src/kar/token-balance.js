18150939c2ec388f423499111d85e1b0b74246c3
Yongfeng LI <wliyongfeng@gmail.com>
2022-02-17 18:27:14 +0800

    Update

--- src/kar/token-balance.js
+++ src/kar/token-balance.js
@@ -3,11 +3,17 @@ const { getKarApi } = require("./api");
 ;(async () => {
   const api = await getKarApi();
 
-  debugger
   const account = '5Dw5KnvTs96FaRQFez1Su15XMMJ65QAi4F1ugNBaXUBiGbX6';
   const b = await api.derive.currencies.balance(account, { Token: 'ACA' })
-  // const b = await api.query.tokens.accounts('5Dw5KnvTs96FaRQFez1Su15XMMJ65QAi4F1ugNBaXUBiGbX6', { ForeignAsset: 0 });
-  console.log(b);
+  const rmrkBalance = await api.query.tokens.accounts('5Dw5KnvTs96FaRQFez1Su15XMMJ65QAi4F1ugNBaXUBiGbX6', { ForeignAsset: 0 });
+  console.log(rmrkBalance)
+
+  api.query.tokens.accounts('5Dw5KnvTs96FaRQFez1Su15XMMJ65QAi4F1ugNBaXUBiGbX6', { Token: 'ACA' })
+    .then(r => {
+      console.log(r)
+      // const aa = r.toHuman();
+      // console.log(JSON.stringify(aa))
+    })
 
   process.exit(0);
 })()
