77c9e256769d9fa573f952bea3be325f7ad8a4af
Yongfeng LI <wliyongfeng@gmail.com>
2022-05-10 14:17:26 +0800

    Update

---
+++ src/ksm/democracy/referenda.js
@@ -0,0 +1,13 @@
+const { getApi } = require("../api");
+
+;(async () => {
+  const api = await getApi();
+  const height = 1574000;
+  const blockHash = await api.rpc.chain.getBlockHash(height);
+  const blockApi = await api.at(blockHash);
+
+  const raw = await blockApi.query.democracy.referendumInfoOf(42);
+  // const voters = await blockApi.query.democracy.votersFor(0);
+  console.log(raw.toJSON())
+  process.exit(0);
+})();
