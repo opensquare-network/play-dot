dc76f1847d490e6beb7054374dc9f3786ff24155
Yongfeng LI <wliyongfeng@gmail.com>
2022-05-25 18:21:01 +0800

    Update

---
+++ src/khala/api.js
@@ -0,0 +1,36 @@
+const { ApiPromise, WsProvider } = require("@polkadot/api");
+const { khalaOptions } = require("@osn/provider-options");
+
+let provider = null;
+let api = null;
+
+const polkadotEndPoint = "wss://khala.api.onfinality.io/public-ws";
+
+async function init() {
+  provider = new WsProvider(polkadotEndPoint);
+  api = await ApiPromise.create({
+    provider,
+    ...khalaOptions,
+  });
+}
+
+async function getApi() {
+  if (!api) {
+    await init()
+  }
+
+  return api;
+}
+
+async function getProvider() {
+  if (!provider) {
+    await init();
+  }
+
+  return provider;
+}
+
+module.exports = {
+  getApi,
+  getProvider,
+}
