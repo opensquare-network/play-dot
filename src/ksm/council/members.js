e7a7d05500c6fe5058c704b357f5725c247dfc71
Yongfeng LI <wliyongfeng@gmail.com>
2022-07-20 18:59:18 +0800

    Test council members

---
+++ src/ksm/council/members.js
@@ -0,0 +1,31 @@
+const { getApi } = require("../api");
+const diff = require("lodash.difference");
+
+async function getMembers(height) {
+  const api = await getApi();
+  const blockHash = await api.rpc.chain.getBlockHash(height);
+  const blockApi = await api.at(blockHash);
+
+  const raw = await blockApi.query.council.members();
+  return raw.toJSON()
+}
+
+async function compare(height) {
+  const oldMembers = await getMembers(height - 1);
+  console.log(oldMembers, oldMembers);
+  // console.log('has jam', oldMembers.includes('H9eSvWe34vQDJAWckeTHWSqSChRat8bgKHG39GC1fjvEm7y'))
+  const newMembers = await getMembers(height);
+
+  const removedOnes = diff(oldMembers, newMembers);
+  const addedOnes = diff(newMembers, oldMembers);
+
+  console.log('removedOnes', removedOnes);
+  console.log('addedOnes', addedOnes);
+}
+
+;(async () => {
+  const members = await getMembers(54289);
+  console.log(members)
+
+  process.exit(0);
+})();
