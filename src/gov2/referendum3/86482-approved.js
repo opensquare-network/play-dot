f090d998ffc49cc9f7a6d72e5ea25a4c7736a9a8
Yongfeng LI <wliyongfeng@gmail.com>
2022-10-31 18:32:01 +0800

    Update

---
+++ src/gov2/referendum3/86482-approved.js
@@ -0,0 +1,12 @@
+// referendumInfoOf.unwrap().asOngoing.toJSON()
+const value = [
+  86482,
+  {
+    "who": "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY",
+    "amount": 100000000000000
+  },
+  {
+    "who": "5GNJqTPyNqANBkUVMN1LPPrxXnFouWXoe2wNSmmEoLctxiZY",
+    "amount": "0x000000000000000000b1a2bc2ec50000"
+  }
+]
--- src/gov2/referendum3/index.js
+++ src/gov2/referendum3/index.js
@@ -2,7 +2,7 @@ const { getApi } = require("../api");
 
 ;(async () => {
   const api = await getApi();
-  const height = 82406;
+  const height = 86482;
   const blockHash = await api.rpc.chain.getBlockHash(height);
   const blockApi = await api.at(blockHash);
 
