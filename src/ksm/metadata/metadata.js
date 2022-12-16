f85687aeae619bce73dc059acb8432e72a84d91e
Yongfeng LI <wliyongfeng@gmail.com>
2022-12-16 15:50:51 +0800

    Update

--- src/ksm/metadata/metadata.js
+++ src/ksm/metadata/metadata.js
@@ -2,7 +2,8 @@ const { getApi } = require("../api");
 
 ;(async () => {
   const api = await getApi();
-  const height = 1517438;
+  // const height = 15426832;
+  const height = 15682703;
   const blockHash = await api.rpc.chain.getBlockHash(height);
   // const metadata = await api.rpc.state.getMetadata(blockHash)
   // console.log(metadata.toJSON())
