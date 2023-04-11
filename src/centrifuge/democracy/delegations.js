81e2133109602b01c7cc577edc789ab4f7f8ddfc
Yongfeng LI <wliyongfeng@gmail.com>
2023-04-11 15:50:04 +0800

    Update

--- src/centrifuge/democracy/delegations.js
+++ src/centrifuge/democracy/delegations.js
@@ -1,4 +1,5 @@
 const { getApi } = require("../api");
+const BigNumber = require("bignumber.js");
 
 function getAddress(storageKey, api) {
   let pubKeyU8a;
@@ -18,17 +19,41 @@ function getAddress(storageKey, api) {
   const voting = await api.query.democracy.votingOf.entries();
   const allDelegations = voting.filter(item => item[1].isDelegating);
 
+  const delegatedMap = {};
+
   const value = allDelegations.map(([who, value]) => {
     const delegator = getAddress(who, api);
     const delegating = value.asDelegating;
     const delegatee = delegating.target.toString();
     const balance = delegating.balance.toString();
+
+    const delegated = delegatedMap[delegatee];
+    if (delegated) {
+      delegatedMap[delegatee] = new BigNumber(delegated).plus(balance).div(Math.pow(10, 18)).toString();
+    } else {
+      delegatedMap[delegatee] = new BigNumber(balance).div(Math.pow(10, 18)).toString();
+    }
+
     return {
       delegator,
       delegatee,
       balance,
     }
   });
+  // console.log(value);
 
-  console.log(value);
+  const delegatedArr = Object.entries(delegatedMap).map(([address, value]) => {
+    return {
+      address,
+      value,
+    }
+  });
+
+  // calc ranked 
+  delegatedArr.sort(({ value: a }, { value: b }) => {
+    return new BigNumber(b).gt(a) ? 1 : -1;
+  })
+  console.log(delegatedArr);
+
+  process.exit(0);
 })();
