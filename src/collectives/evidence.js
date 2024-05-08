3bbc99072fef0f28c11440380c603ef17737e080
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-08 16:25:49 +0800

    Update

--- src/collectives/evidence.js
+++ src/collectives/evidence.js
@@ -3,8 +3,19 @@ const { getApi } = require("./api");
 async function entries() {
   const api = await getApi();
   const entries = await api?.query?.fellowshipCore?.memberEvidence.entries();
-  for (const [storageKey, storageValue] of entries) {
-    console.log(storageKey, storageValue);
+  for (const [storageKey, optionalStorage] of entries) {
+    const address = storageKey.args[0].toString();
+    let evidence, wish;
+    if (optionalStorage.isSome) {
+      const storage = optionalStorage.unwrap();
+      wish = storage[0].toString();
+      evidence = storage[1].toJSON();
+    } else {
+      wish = null;
+      evidence = null;
+    }
+
+    console.log(address, wish, evidence);
   }
 }
 
