6537c9a0c0c78886d56217663f57b0b7c787dab3
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-28 16:06:11 +0800

    Update

--- src/ipfs/multiformats/cid.js
+++ src/ipfs/multiformats/cid.js
@@ -1,8 +1,8 @@
-const { CID } = require("multiformats");
-
-;(() => {
+;(async () => {
+  const { CID } = await import("multiformats");
   const isCid = CID.parse("QmaK8dXReqCeMmM2wKmF7N6bjKauW9SBWe6vh2kPR7D9PQ")
+  const isCid2 = CID.parse("bafkreie42easioag2shgdozwgitvey5gjq7wr3jn24xohuwmipdlwyempq")
   debugger
 
-  console.log('isCid', isCid)
+  console.log('isCid', "abc", isCid)
 })()
