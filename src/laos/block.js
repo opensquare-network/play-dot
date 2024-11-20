3ef8ab49b34fcb9b8f94711cc9e21c79bee77798
Yongfeng LI <wliyongfeng@gmail.com>
2024-11-20 15:51:09 +0800

    Update

--- src/laos/block.js
+++ src/laos/block.js
@@ -2,7 +2,7 @@ const { getCommonApi } = require("../common/api");
 
 (async () => {
   const api = await getCommonApi("wss://rpc.laos.laosfoundation.io/");
-  const blockHash = await api.rpc.chain.getBlockHash(54521);
+  const blockHash = await api.rpc.chain.getBlockHash(934597);
   const block = await api.rpc.chain.getBlock(blockHash);
   const allEvents = await api.query.system.events.at(blockHash);
 
